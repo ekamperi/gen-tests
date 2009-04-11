fd2ee68599b57cc3278e32d677780dbaabb4b2de
Stathis Kamperis <ekamperi@gmail.com>
2009-04-11 05:57:48 +0300

    Cleanup a bit

--- chkml.sh
+++ chkml.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-for i in `seq 1 9`; do
+# Check argument count
+if [ $# -ne 2 ];
+then
+    echo 1>&2 Usage: chkml.sh target-dir checker-program
+    exit 1
+fi
+
+# For every section, check the respective man pages
+for i in `seq 1 9`;
+do
     echo "--- Scanning section $i ---"
-    find $1 -name "*.$i" -exec ./chkml {} \;
+    find $1 -name "*.$i" -exec $2 {} \;
 done
