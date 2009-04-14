e8c8d9e7ae9310fdc71a62ab2d5a50d35bddc6fa
Stathis Kamperis <ekamperi@gmail.com>
2009-04-14 08:22:40 +0300

    Restructure

--- invchkml.sh
+++ invchkml.sh
@@ -1,66 +1,71 @@
 #!/bin/sh
 
-start=1	# section number to start from (1-9)
-end=9	# section number to end to (1-9)
-mandir=/usr/share/man	# directory where the man pages reside
+START=1
+END=9
+MANDIR=/usr/share/man
 
-usage()
-{
-    printf "Usage: %s: [-s start] [-e end] [-v] [-h] man-dir\n" \
-	$(basename $0) >&2
-    exit 1
-}
-
-# Parse user supplied arguments
-while getopts "s:e:vh" f
-do
-    case $f in
-  s)
-      start=$OPTARG
-      ;;
-  e)
-      end=$OPTARG
-      ;;
-  v)
-      verbose=$f
-      ;;
-  h)
-      usage
-      ;;
-  \?)
-      usage
-      ;;
-    esac
-done
-shift `expr $OPTIND - 1`
-
+# MLINK is valid, if man page references the caller.
 validate_file()
 {
-    section=`basename $1 | awk -F '.' '{ print $(NF-1) }'`
-    manpage=`basename $1 | awk -F '.' '{ sub(/\.[^.]*\.[^.]*$/, ""); print }'`
+    SECTION=`basename $1 | awk -F '.' '{ print $(NF-1) }'`
+    MANPAGE=`basename $1 | awk -F '.' '{ sub(/\.[^.]*\.[^.]*$/, ""); print }'`
 
-    isrefd=`man $section $manpage | col -b | grep $manpage`
-    echo "man $section $manpage | col -b | grep $manpage"
-    if [ ! -z "$isrefd" ];
+    ISREFD=`man $SECTION $MANPAGE | col -b | grep $MANPAGE`
+    #echo "man $SECTION $MANPAGE | col -b | grep $MANPAGE"
+    if [ ! -z "$ISREFD" ];
     then
 	return
     fi
 
-    echo "Possibly bogus MLINK to $manpage($section)"
+    echo "Possibly bogus MLINK to $MANPAGE($SECTION)"
 }
 
-# Validate user input
-[ $start -lt 1    ] && usage
-[ $end   -gt 9    ] && usage
-[ $start -gt $end ] && usage
-[ -z "$mandir"    ] && usage
-
-for i in `seq $start $end`
-do
-    echo "--- Scanning section  $i ---"
-    for j in `find "$mandir" -name "z*.$i.gz"`
+# Scan a directory hierarchy and validate all man page files in it.
+scan_path()
+{
+    for i in `seq $START $END`
     do
-	echo "Validating $j"
-	validate_file "$j"
+	echo "--- Scanning section  $i ---"
+	for j in `find "$MANDIR" -name "*.$i.gz"`
+	do
+	    #echo "Validating $j"
+	    validate_file "$j"
+	done
     done
+}
+
+# Print usage information.
+usage()
+{
+    echo "usage: `basename $0` [-s start] [-e end] [-v] [-h] man-dir\n"
+    exit 1
+}
+
+# Parse user supplied arguments.
+while getopts "s:e:vh" f
+do
+    case $f in
+	s)
+	    START=$OPTARG
+	    ;;
+	e)
+	    END=$OPTARG
+	    ;;
+	v)
+	    VERBOSE=$f
+	    ;;
+	h)
+	    usage
+	    ;;
+	\?)
+	    usage
+	    ;;
+    esac
 done
+shift `expr $OPTIND - 1`
+
+# Make sure user input is valid.
+[ $START -lt 1    ] && usage
+[ $END   -gt 9    ] && usage
+[ $START -gt $END ] && usage
+[ -z "$MANDIR"    ] && usage
