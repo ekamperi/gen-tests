308254bf982cb6a1323e7f0ec20a50acd51f19dd
Stathis Kamperis <beket@sadness.(none)>
2009-04-23 18:23:16 +0000

    Add blacklist support

--- checkml.sh
+++ checkml.sh
@@ -7,6 +7,9 @@ verbose=   # verbose level
 srcdir=    # directory where then man pages reside
 validator= # path to the actual program that does the validation
 
+# Any path that matches any string of this list, is excluded from validation
+blacklist="contrib getmntopts"
+
 usage()
 {
     printf "Usage: %s: [-s start] [-e end] [-v] [-h] src-dir validator\n" \
@@ -44,9 +47,14 @@ validator=$2
 [ -z "$srcdir"    ] && usage
 [ -z "$validator" ] && usage
 
+# Escape blacklist, so that grep(1) can process it.
+blacklist=`echo "$blacklist" | sed 's/^/\\\(/g'`  # prepend \(
+blacklist=`echo "$blacklist" | sed 's/$/\\\)/g'`  # append  \)
+blacklist=`echo "$blacklist" | sed 's/\ /\\\|/g'` # replace spaces with \| infix operator
+
 # For every section, validate the associated man pages
 for i in `seq $start $end`;
 do
     echo "--- Scanning section $i ---"
-    find "$srcdir" -name "*.$i" -exec "$validator" {} \;
+    find "$srcdir" -name "*.$i" | grep -v "$blacklist" | xargs -n1 "$validator"
 done
