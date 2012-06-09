5e139b51fd3738815dc35165be35d567e97606cb
Stathis Kamperis <ekamperi@gmail.com>
2012-06-09 21:01:42 +0300

    compnotes.sh: Add versioning to dtrace.notes

--- geant4/compnotes.sh
+++ geant4/compnotes.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 set -e
+set -x
 
 REMOTE_BASE_URL="beket@leaf.dragonflybsd.org:~/public_html/geant4"
+ASCIIDOC=( asciidoc -a data-uri
+		   -a icons
+		   -a toc
+		   -a iconsdir=/usr/share/asciidoc/icons
+		   -a stylesdir=~/adoc-themes/st\ylesheets
+		   -a theme=handbookish )
 
-asciidoc  dtrace.notes
-asciidoc solaris.notes
+GITVERS=$(git rev-list --all | wc -l)
+GITHASH=$(git rev-list --all | head -n1 | cut -c1-5)
 
-scp  dtrace.html "$REMOTE_BASE_URL"
+sed "s/@@version@@/0.${GITVERS}-${GITHASH}/" dtrace.notes > dtrace.notes2
+
+"${ASCIIDOC[@]}" dtrace.notes2
+"${ASCIIDOC[@]}" solaris.notes
+
+scp  dtrace.html "$REMOTE_BASE_URL"/dtrace.html
 scp solaris.html "$REMOTE_BASE_URL"
