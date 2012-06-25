c1ad84a44930e4aae775b07deb14e80568368d4e
Stathis Kamperis <ekamperi.gmail.com>
2012-06-25 18:05:50 +0300

    x64l2{dc,ic}misses: Bind to CPU #2

--- geant4/x64l2icmisses.sh
+++ geant4/x64l2icmisses.sh
@@ -33,7 +33,13 @@ fi
 pics='IC_itlb_L1_miss_L2_hit'          # IC L2 hits
 pics=$pics,'IC_itlb_L1_miss_L2_miss'   # IC L2 misses
 
-/usr/bin/cputrack -c $pics,umask=31 -T 0.1 "$@" |
+/usr/bin/cputrack -c $pics,umask=31 -T 0.2 "$@" |
+{
+    if ((quiet)); then
+	pbind -b2 $(pgrep full_cms) > /dev/null;
+    else
+	pbind -b2 $(pgrep full_cms);
+    fi
     awk -v quiet=$quiet 'BEGIN {
 	if (!quiet) {
 	    printf "%16s %16s %8s\n", "IC Hits", "IC Misses", "% Misses/Hits";
@@ -59,3 +65,4 @@ pics=$pics,'IC_itlb_L1_miss_L2_miss'   # IC L2 misses
 	if (!quiet)
 	    printf "skipped = %d\n", skipped
     }'
+}
