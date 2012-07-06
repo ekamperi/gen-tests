0b968291bf0a76287d06f9b8614a561f208045a8
Stathis Kamperis <ekamperi.gmail.com>
2012-07-06 04:16:45 +0300

    Initial import of *flame.sh files

---
+++ geant4/bmpflame.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# USAGE: bmpflame outfile /path/to/binary args
+#
+
+set -e
+set -x
+
+function usage()
+{
+    echo "usage: $(basename $0) outfile /path/to/binary args" >&2
+    exit 1
+}
+
+if [ "$#" -lt 2 ]; then
+    usage
+fi
+
+outfile="$1"
+shift
+
+binary=$(basename $1)
+title="Flame Graph of ${binary} branch mispredictions - ${outfile}"
+
+dtrace -x ustackframes=100 -x strsize=5000 -n '
+BEGIN
+{
+	tracing = 0;
+}
+
+pid$target::*DoEventLoop*:entry
+{
+	tracing = 1;
+}
+
+pid$target::*DoEventLoop*:return
+{
+	exit(0);
+}
+
+cpc:::FR_retired_branches_mispred-user-5000
+/pid==$target/
+{
+	@[ustack()] = count();
+}' -c "$*" -o "$outfile"
+
+./stackcollapse.pl "$outfile"    |
+c++filt -n -p			 |
+grep ProcessOneTrack             |
+sed "s/${binary}\`//g"           |
+grep -v StatAccepTest            |
+./flamegraph.pl			 |
+sed "s/Flame Graph/${title}/g" > "${outfile}.svg"
