acf806e9920612d6371f754c93d19fce98ddaaba
Stathis Kamperis <ekamperi@gmail.com>
2012-07-29 14:24:34 +0300

    compnotes.sh: Quote array

--- geant4/compnotes.sh
+++ geant4/compnotes.sh
@@ -3,13 +3,13 @@
 set -e
 #set -x
 
-REMOTE_BASE_URL="stathis@island.quantumachine.net:~/public_html/geant4"
+remote_base_url="stathis@island.quantumachine.net:~/public_html/geant4"
 
 if [ $# -eq 1 ]; then
-    REMOTE_BASE_URL="${REMOTE_BASE_URL}/run-$1"
+    remote_base_url="${remote_base_url}/run-$1"
 fi
 
-ASCIIDOC=( asciidoc -a icons
+asciidoc=( asciidoc -a icons
 		   -a toc
 		   -a toclevels=4
 		   -a numbered
@@ -18,22 +18,22 @@ ASCIIDOC=( asciidoc -a icons
 		   -a stylesdir=~/adoc-themes/stylesheets
 		   -a theme=handbookish )
 
-GITVERS=$(git rev-list --all | wc -l)
-GITHASH=$(git rev-list --all | head -n1 | cut -c1-5)
+gitvers=$(git rev-list --all | wc -l)
+githash=$(git rev-list --all | head -n1 | cut -c1-5)
 
-FILES=(dtrace README solaris smartstack)
+files=(dtrace README solaris smartstack)
 
-for file in ${FILES[@]}
+for file in ${files[@]}
 do
     echo "-> Processing document for '${file}'"
 
     # Add the version number to the documents
-    sed "s/@@version@@/0.${GITVERS}-${GITHASH}/" \
+    sed "s/@@version@@/0.${gitvers}-${githash}/" \
 	"${file}.notes" > "${file}.notes2"
 
     # Asciidoc-ify them
-    "${ASCIIDOC[@]}" "${file}.notes2"
+    "${asciidoc[@]}" "${file}.notes2"
 
     # Upload to leaf
-    scp "${file}.html" "$REMOTE_BASE_URL"/
+    scp "${file}.html" "$remote_base_url"/
 done
