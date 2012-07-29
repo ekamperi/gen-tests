1af3d148e84b5ffe92aac49172b99b6a523d58ab
Stathis Kamperis <ekamperi@gmail.com>
2012-07-29 14:24:47 +0300

    finalize.sh: Quote array

--- geant4/finalize.sh
+++ geant4/finalize.sh
@@ -55,10 +55,10 @@ wget -q -r -l1 -R index.html --no-directories -np	\
     ############################################################################
     #				Upload the results			       #
     ############################################################################
-    FILES=(rcmpevts.png rcmpevts2.png cmpcpits.png cmpdcmts.png cmpicmts.png
+    files=(rcmpevts.png rcmpevts2.png cmpcpits.png cmpdcmts.png cmpicmts.png
 	   histcmpevts.png histcmpcpi.png histcmpdcm.png histcmpicm.png)
 
-    for file in "${FILES[@]}"
+    for file in "${files[@]}"
     do
 	echo "-> Uploading file '${file}'"
 	scp "${file}" "${user}@${host}:${remote_base_path}/run-${iteration}"
