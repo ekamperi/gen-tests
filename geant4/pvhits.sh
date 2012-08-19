6df2379aadf6e51346681ed11ad910c7f95696fb
Stathis Kamperis <ekamperi@gmail.com>
2012-08-19 06:00:40 +0300

    pvhits: Document. Document. Document.

--- geant4/pvhits.sh
+++ geant4/pvhits.sh
@@ -9,7 +9,26 @@ if [ ! $# -eq 1 ]; then
     exit 1
 fi
 
-# Calculate the hits / misses ratio
+# Calculate the hits / misses ratio in the G4PhysicsVector::Value()
+# function. The file with the values is generated as per the following
+# DTrace invocation. Mind that the address '0xc0' inside Value()
+# function needs to be determined manually via diassembling.
+#
+# dtrace -qn 'pid$target::_ZN15G4PhysicsVector5ValueEd:c0
+# {
+#     @branch = count();
+# }
+#
+# pid$target::_ZN15G4PhysicsVector5ValueEd:entry
+# {
+#     @total = count();
+# }
+#
+# tick-100ms
+# {
+#     printa(@branch);
+#     printa(@total);
+# }' -c '/home/stathis/geant4.9.5.p01/bin/Linux-g++/full_cms ./bench1_5k.g4' -o values
 
 awk '{
     if ($1) {
@@ -35,7 +54,7 @@ df <- read.table('$1.dat')
 p <- ggplot(df) + geom_line(aes(x=1:length(df\$V1), y=df\$V1))
 p <- p + xlab("simulation time (0.1sec)")
 p <- p + ylab("% hits / misses")
-p <- p + opts(title="The evolution of hits/misses ratio in G4PhysicsVector::Value()\n(1000 simulated events)")
+p <- p + opts(title="The evolution of hits/misses ratio in G4PhysicsVector::Value()\n(5000 simulated events)")
 p
 dev.off()
 EOF
@@ -44,4 +63,3 @@ cat "$1.rplot" | R --vanilla --slave
 
 display "$1.png"
 exit 0
-
