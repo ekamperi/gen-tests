2450cc25ca8f7f2bfec4a93f0bdfb3aadc15a4a7
Stathis Kamperis <ekamperi@gmail.com>
2012-09-26 16:42:15 +0300

    clone-git-repos.sh: Add bitbucket support

--- clone-git-repos.sh
+++ clone-git-repos.sh
@@ -3,16 +3,21 @@
 set -e
 set -x
 
-host="git://gitweb.dragonflybsd.org/~beket"
+leafhost="git://gitweb.dragonflybsd.org/~beket"
+bitbhost="https://ekamperi@bitbucket.org/ekamperi"
 reposdir="git-repos"
 
-repos=(
+leafrepos=(
     "conf-files"
     "dtrace-scripts"
     "gen-tests"
     "mathlib"
     "pcca-dbdump"
     "pcca-tests"
+)
+
+bitbrepos=(
+    "e-galinos"
 )
 
 clonerepo()
@@ -25,9 +30,19 @@ updaterepo()
     ( cd "$1" && git pull )
 }
 
-mkdir -p "${reposdir}"
-for repo in "${repos[@]}"
-do
-    echo "-> Fetching repository '${repo}'"
-    clonerepo "${reposdir}" "${host}/${repo}.git" || updaterepo "${reposdir}/${repo}"
-done
+# First argument is host, and second the array with the repos
+dohost()
+{
+    local host="$1"
+    shift
+
+    mkdir -p "${reposdir}"
+    for repo in "${@}"
+    do
+	echo "-> Fetching repository '${repo}'"
+	clonerepo "${reposdir}" "${host}/${repo}.git" || updaterepo "${reposdir}/${repo}"
+    done
+}
+
+dohost "${leafhost}" "${leafrepos[@]}"
+dohost "${bitbhost}" "${bitbrepos[@]}"
